7fe0a4a939eadcf615345583617074e69c626e1f
mobina kargar <dymamsijhidjj@gmail.com>
2023-06-07 16:13:24 +0330

    Sending the code for checking....

--- src/main/java/com/example/spotify/DataBase/Playlist.java
+++ src/main/java/com/example/spotify/DataBase/Playlist.java
@@ -1,4 +1,75 @@
 package com.example.spotify.DataBase;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Playlist {
+
+    private UUID playlistID;
+    private String title;
+    private String description;
+    private User creator;
+    private int numLikes;
+    private List<Music> tracks;
+
+    // constructor
+    public Playlist(UUID playlistID, String title, String description, User creator, int numLikes) {
+        this.playlistID = playlistID;
+        this.title = title;
+        this.description = description;
+        this.creator = creator;
+        this.numLikes = numLikes;
+        this.tracks = new ArrayList<>();
+    }
+
+    // getters and setters
+
+    public UUID getPlaylistID() {
+        return playlistID;
+    }
+
+    public void setPlaylistID(UUID playlistID) {
+        this.playlistID = playlistID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public int getNumLikes() {
+        return numLikes;
+    }
+
+    public void setNumLikes(int numLikes) {
+        this.numLikes = numLikes;
+    }
+
+    public List<Music> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Music> tracks) {
+        this.tracks = tracks;
+    }
 }
---
+++ src/main/java/com/example/spotify/picOfadd.java
@@ -0,0 +1,26 @@
+package com.example.spotify;
+
+import com.example.spotify.DataBase.Playlist;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+
+public class picOfadd {
+
+    @FXML
+    private Button click;
+
+    @FXML
+    void click(ActionEvent event) {
+
+    }
+    void setSecondData(Playlist playlist){
+
+    /*    playlist.setText(playlist.getTitle());
+        genre.setText(artist.getGenre());
+        instagram.setText(artist.getInstagram());
+        twitter.setText(artist.getTwitter());*/
+    }
+
+}
